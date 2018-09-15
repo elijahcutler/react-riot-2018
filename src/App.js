c3aad0f03bdc8ce32eea3f7995ef19828673786b
Kirin Patel <kirinpatel@gmail.com>
2018-09-15 02:20:18 -0400

    Added Firebase to project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import logo from './logo.svg';
-import './App.css';
+import firebase from './Components/firebase';
 
 class App extends Component {
+  state = {};
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <Switch>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
