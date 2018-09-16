85215909a0010cf6adb5ff38752d39063d5bcf64
Kirin Patel <kirinpatel@gmail.com>
2018-09-15 21:56:47 -0400

    Changed request to use cloud function url

--- web/src/Components/Events.js
+++ web/src/Components/Events.js
@@ -15,7 +15,7 @@ export default class extends Component {
   }
 
   fetchTimeline = (page = 0) => {
-    axios.get('http://localhost:5000/gittogether-6f7ce/us-central1/getGlobalTimeline', {
+    axios.get('https://us-central1-gittogether-6f7ce.cloudfunctions.net/getGlobalTimeline', {
       headers: {
         authorization: `Bearer ${this.state.idToken}`
       }
